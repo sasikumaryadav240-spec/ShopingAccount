e57f57a57461d5eeb4015b60c7f034f839974a10
shasi kumar <shasikumar@varnarc.com>
2026-05-16 13:48:58 +0530

    New Updates

--- router/historyRouter.js
+++ router/historyRouter.js
@@ -3,5 +3,5 @@ import express from "express";
 import { getCombinedMonthlyReport, historyLogic} from "../GlobalLogic/historyLogic.js";
 export const historyRouter = express.Router();
 
-historyRouter.get("history", historyLogic);
-historyRouter.get("monthCombinedHistory", getCombinedMonthlyReport);
+historyRouter.get("/history", historyLogic);
+historyRouter.get("/monthCombinedHistory", getCombinedMonthlyReport);
